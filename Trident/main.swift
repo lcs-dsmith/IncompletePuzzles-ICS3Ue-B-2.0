eaeae6c57e4197175d6e2f4d48aad7c370e65567
lcs-dsmith <dsmith@lcs.on.ca>
2021-01-28 09:30:17 -0500

    Prints the three tines

--- Trident/main.swift
+++ Trident/main.swift
@@ -50,17 +50,40 @@ let S = Int(readLine()!)!
 //}
 //
 
-// Draw one time
-//for _ in 1...T {
-//    print("*")
-//}
+// Draw all tines
+for _ in 1...T {
+    
+    // Draw first line of all tines
+    for _ in 1...3 {
+        print("*", terminator: "")
+
+        for _ in 1...S {
+
+            print("s", terminator: "")
+        }
+    }
+
+// Row compleate .. next line
+print("")
+
+}
 
 // Draw first line of all three tines with space between them
-for _ in 1...3 {
-    print("*", terminator: "")
-    
-    for _ in 1...S {
-    
-        print("s", terminator: "")
-    }
-}
+//for _ in 1...3 {
+//    print("*", terminator: "")
+//
+//    for _ in 1...S {
+//
+//        print("s", terminator: "")
+//    }
+//}
+
+// Draw th tines with the spaces
+//    for _ in 1...3 {
+//        print("*", terminator: "")
+//
+//        for _ in 1...S {
+//
+//            print("s", terminator: "")
+//        }
+//    }
