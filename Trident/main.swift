642ee97d8b17e0ba799e07caaac933afa7f32c61
lcs-dsmith <dsmith@lcs.on.ca>
2021-01-28 09:47:41 -0500

    Will draw a trident in interactive mode

--- Trident/main.swift
+++ Trident/main.swift
@@ -25,10 +25,7 @@ print("Enter tine spacing:")
 print("   ", terminator: "")
 let S = Int(readLine()!)!
 
-// Get Handle length
-//print("Enter Handle length:")
-//print("   ", terminator: "")
-//let H = Int(readLine()!)!
+
 // OUTPUT
 
 //// Produce top of trident according to length given
@@ -49,30 +46,39 @@ let S = Int(readLine()!)!
 //    print("")
 //}
 //
-
+//
 // Draw all tines
 for _ in 1...T {
     
     // Draw first line of all tines
     for _ in 1...3 {
         print("*", terminator: "")
-
+        
         for _ in 1...S {
-
+            
             print(" ", terminator: "")
         }
     }
-
-// Row compleate .. next line
-// The \n character sequence
-// pushes the content down to the next line
+    
+    // Row compleate .. next line
+    // The \n character sequence
+    // pushes the content down to the next line
     print("\n", terminator: "")
 }
-    
-    for _ in 1...3 + S + S  {
-        print("*", terminator: "")
-    }
+// Draw the Haft
+for _ in 1...3 + S + S  {
+    print("*", terminator: "")
+}
+//go to next line
+print("\n", terminator: "")
 
+// Draw the handle
+for _ in 1...4 {
+    for _ in 1...1+S {
+        print(" ",terminator: "")
+    }
+    print("*")
+}
 
 
 // Draw first line of all three tines with space between them
