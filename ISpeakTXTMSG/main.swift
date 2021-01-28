018db58c4964974bde40286d4a7400a0c931e7ac
lcs-dsmith <dsmith@lcs.on.ca>
2021-01-28 08:38:52 -0500

    Done Shifty Sums

--- ISpeakTXTMSG/main.swift
+++ ISpeakTXTMSG/main.swift
@@ -19,20 +19,83 @@ print("==============")
 print("Enter phrase> ", terminator: "")
 let phrase = readLine()!
 
-// PROCESS
-func translate(phrase shortForm: String) -> String {
+//while phrase != "Goodbye" {
+//    print("Enter phrase> ", terminator: "")
+//    let phrase = readLine()!
 
-    // NOTE: Instead of an "if statement" consider using a different type of Swift structure to handle all the different possible cases...
-    if phrase == "TA" {
-        return "totally awesome"
-    } else if phrase == "TTYL" {
-        return "talk to you later"
-    } else {
-        return phrase
+    // PROCESS
+    // Paramiters are inputs to the function
+    // Paramiters are separated by ","
+    // A paramiter has an external and internal name
+    // In this code translate has one paramiter
+    // the external name is "phrase"
+    // the internal name is "shortForm"
+    func translate(phrase shortForm: String) -> String {
+        
+        
+        
+        // NOTE: Instead of an "if statement" consider using a different type of Swift structure to handle all the different possible cases...
+        switch shortForm {
+        case "TA":
+            return("totally awesome")
+        case "TTYL":
+            return("talk to you later")
+        case "CU":
+            return("see you")
+        case ":-)":
+            return("I'm happy")
+        case ":-(":
+            return("I'm unhappy")
+        case ";-)":
+            return("wink")
+        case ":-P":
+            return("stick my tongue out")
+        case "(˜.˜)":
+            return("sleepy")
+        case "CCC":
+            return("Canadian Cheese Champion")
+        case "CUZ":
+            return("because")
+        case "TY":
+            return("thank-you")
+        case "YW":
+            return("you’re welcome")
+        case "totally awesome" :
+            return("TA")
+        case "talk to you later":
+            return("TTYL")
+        case "see you":
+            return("CU")
+        case "I'm happy":
+            return(":-)")
+        case "I'm unhappy":
+            return(":-(")
+        case "wink":
+            return(";-)")
+        case "stick my tongue out":
+            return(":-P")
+        case "sleepy":
+            return("(˜.˜)")
+        case "Canadian Computing Competition":
+            return("CCC")
+        case "because":
+            return("CUZ")
+        case "thank-you":
+            return("TY")
+        case "you’re welcome":
+            return("YW")
+        default:
+            return(shortForm)
+        }
+        return shortForm
     }
+    
+    // OUTPUT
+    // This is the call sight
+    // this is where the function is called or invocked
+    // the external name shows at the call site
+    // What we "pass in" for a paramiter is the argument
+    // Paramiter == question
+    let output = translate(phrase: phrase)
+    
 
-}
-
-// OUTPUT
-let output = translate(phrase: phrase)
-print(output)
--- ShiftySums/main.swift
+++ ShiftySums/main.swift
@@ -16,18 +16,37 @@ print("===========")
 // INPUT
 
 // Get the number to be shifted
+print("Base")
 let n = Int(readLine()!)!
 
 // How many times should we shift?
-let k = 3
+print("Shift")
+let k = Int(readLine()!)!
 
 // PROCESS
-
+func shift(n:Int, k: Int) -> Int {
+    var output = n
+    if k != 0{
+        for i in 1...k {
+            var toAdd = n
+            for _ in 1...i {
+                toAdd *= 10
+            }
+            output += toAdd
+        }
+    } else {
+        output = n
+    }
+    return output
+    
+}
+let answer = shift(n: n, k: k)
+print(answer)
 // Shift the given number "k" number of times
-var output = n
-output += n * 10
-output += n * 10 * 10
-output += n * 10 * 10 * 10
+//var output = n
+//output += n * 10
+//output += n * 10 * 10
+//output += n * 10 * 10 * 10
 
 // OUTPUT
-print(output)
+//print(output)
