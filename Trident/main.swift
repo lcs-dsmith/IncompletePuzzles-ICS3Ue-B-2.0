0871759d9ca5cea1f40e422a5b83bf8a71d9c471
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-01 11:06:35 -0500

    Finished project

--- Trident/main.swift
+++ Trident/main.swift
@@ -25,9 +25,59 @@ print("Enter tine spacing:")
 print("   ", terminator: "")
 let S = Int(readLine()!)!
 
+// Get handle length
+print("Enter Handle Length:")
+print("   ", terminator: "")
+let H = Int(readLine()!)!
 
+// PROCESS
+func drawTrident(tineLength T: Int,
+                 tineSpacing S: Int,
+                 handleLength H: Int) -> String {
+    //Create a variabe name for output
+    var output = ""
+    
+    for _ in 1...T {
+        
+        // Draw first line of all tines
+        for _ in 1...3 {
+            output += "*"
+
+            
+            for _ in 1...S {
+                output += " "
+            }
+        }
+        
+        // Row compleate .. next line
+        // The \n character sequence
+        // pushes the content down to the next line
+        output += "\n"
+    }
+    // Draw the Haft
+    for _ in 1...3 + S + S  {
+        output += "*"
+    }
+    //go to next line
+    output += "\n"
+
+    // Draw the handle
+    for _ in 1...H {
+        for _ in 1...1+S {
+            output += " "
+            
+        }
+        output += "*"
+        output += "\n"
+    }
+    return output
+}
+
+let programOutput = drawTrident(tineLength: T, tineSpacing: S, handleLength: H)
+print(programOutput)
 // OUTPUT
-
+//
+//
 //// Produce top of trident according to length given
 //for _ in 1...T {
 //    // Print the tines
@@ -48,39 +98,9 @@ let S = Int(readLine()!)!
 //
 //
 // Draw all tines
-for _ in 1...T {
-    
-    // Draw first line of all tines
-    for _ in 1...3 {
-        print("*", terminator: "")
-        
-        for _ in 1...S {
-            
-            print(" ", terminator: "")
-        }
-    }
-    
-    // Row compleate .. next line
-    // The \n character sequence
-    // pushes the content down to the next line
-    print("\n", terminator: "")
-}
-// Draw the Haft
-for _ in 1...3 + S + S  {
-    print("*", terminator: "")
-}
-//go to next line
-print("\n", terminator: "")
-
-// Draw the handle
-for _ in 1...4 {
-    for _ in 1...1+S {
-        print(" ",terminator: "")
-    }
-    print("*")
-}
-
-
+//
+//
+//
 // Draw first line of all three tines with space between them
 //for _ in 1...3 {
 //    print("*", terminator: "")
@@ -90,7 +110,7 @@ for _ in 1...4 {
 //        print("s", terminator: "")
 //    }
 //}
-
+//
 // Draw th tines with the spaces
 //    for _ in 1...3 {
 //        print("*", terminator: "")
